6f267fea772cda1c4ee2fb7de00e565e30c0c2f7
Yan Silva <yansilva@MacBook-Pro-de-Yan.local>
2020-10-07 18:17:12 -0300

    First commit - Upload solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import Input from './Form/Input';
 import './App.css';
+import useForm from './Hooks/useForm';
 
-function App() {
+const App = () => {
+  const cep = useForm('cep');
+  const email = useForm('email');
+  const nome = useForm();
+  // Campos não necessários
+  // Exemplos
+  const sobrenome = useForm(false);
+
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    if (cep.validate() && email.validate() && nome.validate()) {
+      console.log(cep.value, email.value, nome.value, sobrenome.value); // Req is here
+    } else {
+      console.log('Não enviar');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <Input label="Nome" id="nome" type="text" {...nome} />
+
+      <Input label="Sobrenome" id="sobrenome" type="text" {...sobrenome} />
+
+      <Input
+        label="Cep"
+        id="cep"
+        type="text"
+        placeholder="00000-000"
+        {...cep}
+      />
+
+      <Input label="Email" id="email" type="email" {...email} />
+      <button>Enviar</button>
+    </form>
   );
-}
+};
 
 export default App;
---
+++ src/Form/Input.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Input = ({
+  id,
+  label,
+  value,
+  onChange,
+  type,
+  onBlur,
+  placeholder,
+  error,
+}) => {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        name={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        onBlur={onBlur}
+      />
+      {error && <p>{error}</p>}
+    </>
+  );
+};
+
+export default Input;
